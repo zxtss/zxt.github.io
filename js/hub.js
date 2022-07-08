79b3c2b32cd0283f2531992eb15bbc44025438f4
DESKTOP-38MVVFL\Lenovo <1020965670>
2022-07-08 16:48:54 +0800

    no message

---
+++ js/hub.js
@@ -0,0 +1,54 @@
+"use strict";
+var webTitle = "动画特效";
+var dataList = [
+    { title: " 淡入-从上", type: "a-fadeinT" },
+    { title: "淡入-从右", type: "a-fadeinR" },
+    { title: "淡入-从下", type: "a-fadeinB" },
+    { title: "淡入-从左", type: "a-fadeinL" },
+    { title: "淡出-向上", type: "a-fadeoutT" },
+    { title: "淡出-向右", type: "a-fadein" },
+    { title: "淡出-向下", type: "a-fadeoutB" },
+    { title: "淡出-向左", type: "a-fadeoutL" },
+    { title: "弹跳", type: "a-bounce" },
+    { title: "弹入-从上", type: "a-bounceinT" },
+    { title: "弹入-从右", type: "a-bounceinR" },
+    { title: "弹入-从下", type: "a-bounceinB" },
+    { title: "弹入-从左", type: "a-bounceinL" },
+    { title: "弹出-向上", type: "a-bounceoutT" },
+    { title: "弹出-向右", type: "a-bounceoutR" },
+    { title: "弹出-向下", type: "a-bounceoutB" },
+    { title: "弹出-向左", type: "a-bounceoutL" },
+    { title: "转入-从左上", type: "a-rotateinLT" },
+    // { title: "转入-从左下", type: "a-rotateinLB" },
+    { title: "转入-从右上", type: "a-rotateinRT" },
+    { title: "转入-从右下", type: "a-rotateinRB" },
+    { title: "转出-向左上", type: "a-rotateoutLT" },
+    { title: "转出-向左下", type: "a-rotateoutLB" },
+    { title: "转出-向右上", type: "a-rotateoutRT" },
+    // { title: "转出-向右下", type: "a-rotateoutRB" },
+    { title: "翻入-X轴", type: "a-flipinX" },
+    { title: "翻入-Y轴", type: "a-flipin a-flipinY" },
+    { title: "翻出-X轴", type: "a-flipoutX" },
+    { title: "翻出-Y轴", type: "a-flipout a-flipoutY" },
+    { title: "闪烁", type: "a-flash" },
+    { title: "震颤", type: "a-shake" },
+    { title: "摇摆", type: "a-swing" },
+    { title: "摇晃", type: "a-wobble" },
+    { title: "震铃", type: "a-ring" },
+];
+let x;
+x = ['Runoob', 1]; // 元组
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+let c = Color.Blue; //枚举
+const nameSiteMapping = new Map();
+// 设置 Map 对象
+nameSiteMapping.set("Google", 1);
+function greet() {
+    return "Hello World";
+}
---
+++ js/index.js
@@ -0,0 +1,122 @@
+; (function (undefined) {
+    "use strict"
+    var _global;
+
+    function plugin() { }
+
+    // Test data (do not use)
+    plugin.prototype.demo = function () {
+        return { code: 200, msg: '操作成功', data: { type: 1, comment: 12, content: 'The Test data', image: 'https://img1.baidu.com/it/u=722430420,1974228945&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500' } }
+    }
+    plugin.prototype.add = function (a, b) { return a + b }//add
+    plugin.prototype.sub = function (a, b) { return Math.abs(a - b) }//subtraction
+    plugin.prototype.mul = function (a, b) { return a * b }//The multiplication
+    plugin.prototype.div = function (a, b) { return a / b }//division
+    plugin.prototype.rem = function (a, b) { return a % b }//remainder
+    plugin.prototype.getParameter = function () { return getQueryVariable() }//Get all URL parameters
+    plugin.prototype.copy = function (text) { return copyContent(text) }//copy content
+    plugin.prototype.toast = function (text) { return showToast(text) }//Pop-up prompts
+    plugin.prototype.stamp = function (type, time) { return stamp(type, time) }//time<=>timestamp
+    plugin.prototype.userInfo = function () {
+        return {
+            "code": 200,
+            "data": {
+                "name":"patton",
+                "QQ":"1020965670",
+                "email":"1020965670@qq.com"
+            },
+            "message": "success"
+        }
+    }
+    //rolling scolltop
+    plugin.prototype.rolling = function () {
+        var timer = setInterval(function () {
+            var left = window.pageYOffset;
+            var step = Math.ceil((left - 0) / 10);
+            window.scroll(0, left - step);
+            if (left == 0) {
+                clearInterval(timer);
+            }
+        }, 10);
+    }
+
+    //The countdown
+    // plugin.prototype.countdown = function (timestamp) {
+    //  return new Promise((resolve, reject) => {
+    //      resolve()
+    //  })
+    // }
+    //The rolling distance
+    window.onscroll = function () {
+        // console.log(document.documentElement.scrollTop || document.body.scrollTop)
+    }
+    //Gets the parameters after the URL
+    function getQueryVariable() {
+        var url = location.search;
+        var theRequest = new Object();
+        if (url.indexOf("?") != -1) {
+            var str = url.substring(1);
+            var strs = str.split("&");
+            for (var i = 0; i < strs.length; i++) {
+                theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+            }
+        }
+        return theRequest;
+    }
+
+    //showToast
+    function showToast(title) {
+        var div = document.createElement('div');
+        document.body.appendChild(div);
+        div.style = `width: 100px;height: 50px;background: rgba(0,0,0,.5);display: flex;align-items: center;justify-content: center;color: #ffffff;border-radius: 5px;position:fixed;z-inde:999999;top:${document.body.scrollHeight / 2}px;left:${document.body.scrollWidth / 2}px;`
+        div.innerText = title;
+        setTimeout(() => {
+            document.body.removeChild(div);
+        }, 2000);
+    }
+
+    //copy content
+    function copyContent(text) {
+        showToast('复制成功');
+        if (navigator.clipboard) {
+            navigator.clipboard.writeText(text);
+        } else {
+            var textarea = document.createElement('textarea');
+            document.body.appendChild(textarea);
+            textarea.style.position = 'fixed';
+            textarea.style.clip = 'rect(0 0 0 0)';
+            textarea.style.top = '10px';
+            textarea.value = text;
+            textarea.select();
+            document.execCommand('copy', true);
+            document.body.removeChild(textarea);
+        }
+    }
+    //Time and timestamp conversion
+    function stamp(type, time) {
+        let date = time ? new Date(time) : new Date();
+        // time=>timeStamp
+        if (type == 'ts') return Math.round(date.getTime() / 1000);
+        //timeStamp=>time
+        if (type == 'date') {
+            var time = time ? new Date(time) : new Date();
+            var y = time.getFullYear();
+            var M = time.getMonth() + 1;
+            var d = time.getDate();
+            var h = time.getHours();
+            var m = time.getMinutes();
+            var s = time.getSeconds();
+            return y + '-' + (M < 9 ? '0' + M : M) + '-' + (d < 9 ? '0' + d : d) + ' ' + (h < 9 ? '0' + h : h) + ':' + (m <
+                9 ? '0' + m : m) + ':' + (s < 9 ? '0' + s : s);
+        }
+    }
+    //Exposed to global objects(plugin)
+    _global = (function () { return this || (0, eval)('this'); }())
+    if (typeof module !== "undefined" && module.exports) {
+        module.exports = plugin;
+    } else if (typeof define === "function" && define.amd) {
+        define(function () { return plugin; });
+    } else {
+        !('plugin' in _global) && (_global.plugin = plugin);
+    }
+}())
