27d0bc120d77b9327afb606cf064269b8805ae2d
DESKTOP-38MVVFL\Lenovo <1020965670>
2022-08-05 10:15:05 +0800

    增加pyscript

---
+++ python/index.py
@@ -0,0 +1,12 @@
+'''
+Description: 
+Version: 1.0
+Author: 努力才能逃离
+Date: 2022-08-04 11:40:30
+LastEditors: 努力才能逃离
+LastEditTime: 2022-08-04 11:46:39
+'''
+# _*_ coding:utf-8 _*_
+import time
+time.sleep(5000)
+print("你好")
