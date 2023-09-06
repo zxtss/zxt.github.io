1666165db123c55da72b09b93256c9c0991b234b
DESKTOP-38MVVFL\Lenovo <1020965670>
2023-09-06 16:44:18 +0800

    update

---
+++ js/worker.js
@@ -0,0 +1,8 @@
+// 检测是否支持worker
+// if (window.Worker) {
+//     var myWorker = new Worker('./worker.js');
+//     myWorker.onmessage = function (e) {
+//         console.log('接收到消息：', e);
+//     },
+//     postMessage("234234")
+// }
